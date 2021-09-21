f27a24c5f760cf6aa62260f7b496546171fb9eaf
sosow0212 <sosow0212@naver.com>
2021-09-21 20:40:17 +0900

    postman Post로 확인하기

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# npm init > npm install express --save
-# npm install mongoose --save
+# 인프런 존안님 강의
+
+## npm init > npm install express --save
+## npm install mongoose --save
+## ssh 등록
+
+## postman에서 post로 회원가입 확인
+
--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb+srv://sosow0212:1234@cluster0.yzuli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-  .then(() => console.log('MongoDB Connected...'))
-  .catch((e) => console.log('MongoDB error: ', e));
+  .connect(
+    "mongodb+srv://sosow0212:1234@cluster0.yzuli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((e) => console.log("MongoDB error: ", e));
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", (req, res) => {
+  // 회원가입시 필요한 정보를 Client에서 가저오면,
+  // 그것들을 데이터베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
