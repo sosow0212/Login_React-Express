a3ef60e1522beae94e25ad09627d41c995086358
sosow0212 <sosow0212@naver.com>
2021-09-22 19:18:20 +0900

    LoginPage 완성

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../_actions/user_action";
 
-function RegisterPage() {
+function RegisterPage(props) {
+
+  const dispatch = useDispatch();
+
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    let body = {
+      email: Email,
+      password: Password,
+    };
+
+    dispatch(loginUser(body)).then((response) => {
+      if (response.payload.loginSuccess) {
+        // 메인 페이지로 이동
+
+        props.history.push("/");
+      } else {
+        alert("Error");
+      }
+    });
+  };
+
+
   return (
-    <div>
-      RegisterPage
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <br />
+        <button type="submit">Login</button>
+      </form>
     </div>
   )
 }
