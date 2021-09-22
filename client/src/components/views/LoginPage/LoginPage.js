f7b7b41ab4335f8ebd28d12957f3184b992869dc
sosow0212 <sosow0212@naver.com>
2021-09-22 19:41:44 +0900

    회원가입, 로그인 오류 해결

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,65 +1,59 @@
-import React from "react";
-import { useState } from "react";
-import Axios from "axios";
-import { useDispatch } from "react-redux";
+import React, {useState} from 'react';
+import { useDispatch } from "react-redux"; //dispatch를 통해서 action을 취함 , 그리고 redux로 이동함
 import { loginUser } from "../../../_actions/user_action";
-
+import { withRouter } from 'react-router-dom';
 function LoginPage(props) {
-  const dispatch = useDispatch();
+	
+	const dispatch = useDispatch();
 
-  const [Email, setEmail] = useState("");
-  const [Password, setPassword] = useState("");
 
-  const onEmailHandler = (event) => {
-    setEmail(event.target.value);
-  };
+	const [Email, setEmail] = useState("")
+	const [Password, setPassword] = useState("")
 
-  const onPasswordHandler = (event) => {
-    setPassword(event.target.value);
-  };
+	const onEmailHandler = (event) =>{
+		setEmail(event.currentTarget.value)// setEmail을 이용하여 Email의 state를 바꿔준다.
+	}
+	const onPasswordHandler = (event) =>{
+		setPassword(event.currentTarget.value)// setPassword을 이용하여 Password의 state를 바꿔준다.
+	}
+	const onSubmitHandler = (event) =>{
+		event.preventDefault();//밑에 코드를 수행하지 않고 페이지가 새로고침 되버리는 것을 방지함 .
 
-  const onSubmitHandler = (event) => {
-    event.preventDefault();
+		let body = {
+			email : Email,
+			password : Password
+		}
 
-    let body = {
-      email: Email,
-      password: Password,
-    };
+		dispatch(loginUser(body)) // loginUser라는 action
+		.then(response => {
+			if (response.payload.loginSuccess) {
+				props.history.push('/') //로그인 성공시 렌딩페이지 (/, root page)이동
+			} else {
+				alert('Error')
+			}
+		})
+	}
 
-    dispatch(loginUser(body)).then(response => {
-      if (response.payload.loginSuccess) {
-        // 메인 페이지로 이동
-
-        props.history.push("/");
-      } else {
-        alert("Error");
-      }
-    });
-  };
-
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "100vh",
-      }}
-    >
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={onSubmitHandler}
-      >
-        <label>Email</label>
-        <input type="email" value={Email} onChange={onEmailHandler} />
-        <label>Password</label>
-        <input type="password" value={Password} onChange={onPasswordHandler} />
-        <br />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+    return (
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height:'100vh'
+        }}>
+            <form style={{display:'flex', flexDirection:'column'}}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+                
+		<br />
+                <button type ="submit">
+                    Login
+                </button>
+            </form>
+        </div>
+    )
 }
 
-export default LoginPage;
+export default withRouter(LoginPage)
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,92 +1,84 @@
-import React from "react";
-import { useState } from "react";
+import React, {useState} from 'react'
 import { useDispatch } from "react-redux";
 import { registerUser } from "../../../_actions/user_action";
-import Axios from 'axios';
+import { withRouter } from 'react-router-dom';
+
 
 function RegisterPage(props) {
-  const dispatch = useDispatch();
-
-  const [Email, setEmail] = useState("");
-  const [Password, setPassword] = useState("");
-  const [Name, setName] = useState("");
-  const [ConfirmPassword, setConfirmPassword] = useState("");
-
-  const onEmailHandler = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const onPasswordHandler = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const onNameHandler = (event) => {
-    setName(event.target.value);
-  };
-
-  const onConfirmPasswordHandler = (event) => {
-    setConfirmPassword(event.target.value);
-  };
-
-  const onSubmitHandler = (event) => {
-    event.preventDefault();
-
-    if (Password !== ConfirmPassword) {
-      return alert("비밀번호가 일치하는지 확인하세요.");
-    }
-
-    let body = {
-      email: Email,
-      password: Password,
-      name: Name,
-    };
-
-    dispatch(registerUser(body))
-    .then(response => {
-      if(response.payload.success) {
-        props.history.push("/login")
-      } else {
-        alert("Failed to sign up")
-      }
-    })
-  };
+	const dispatch = useDispatch();
 
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "100vh",
-      }}
-    >
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={onSubmitHandler}
-      >
-        <label>Email</label>
-        <input type="email" value={Email} onChange={onEmailHandler} />
+	const [Email, setEmail] = useState("")
+	const [Name, setName] = useState("")
+	const [Password, setPassword] = useState("")
+	const [confirmPassword, setConFirmPassword] = useState("")
 
-        <label>Name</label>
-        <input type="text" value={Name} onChange={onNameHandler} />
+	const onEmailHandler = (event) =>{
+		setEmail(event.currentTarget.value)
+	}
+	const onNameHandler =  (event) => {
+		setName(event.currentTarget.value)
+	}
+	const onPasswordHandler = (event) =>{
+		setPassword(event.currentTarget.value)
+	}
+	const onConfirmPasswordHandler = (event) =>{
+		setConFirmPassword(event.currentTarget.value)
+	}
+	const onSubmitHandler = (event) =>{
+		event.preventDefault();
+		
+		if (Password !== confirmPassword) {
+			return alert("Please check your password.")
+		}
+		
+		let body = {
+			email : Email,
+			password : Password,
+			name: Name
+		}
 
-        <label>Password</label>
-        <input type="password" value={Password} onChange={onPasswordHandler} />
+		dispatch(registerUser(body)) // registerUser Action
+		.then(response => {
+			if (response.payload.success) {
+				props.history.push('/login')
+			} else {
+				alert('Failed to sing up Please check your email.')
+			}
+		})
 
-        <label>ConfirmPassword</label>
-        <input
-          type="password"
-          value={ConfirmPassword}
-          onChange={onConfirmPasswordHandler}
-        />
+	}
 
-        <br />
-        <button type="submit">회원가입</button>
-      </form>
-    </div>
-  );
+
+
+
+
+	return (
+		<div style={{
+			display: 'flex', justifyContent: 'center', alignItems: 'center'
+			, width: '100%', height:'100vh'
+		    }}>
+			<form style={{display:'flex', flexDirection:'column'}}
+			    onSubmit={onSubmitHandler}
+			>
+			    <label>Email</label>
+			    <input type="email" value={Email} onChange={onEmailHandler} />
+			    
+			    <label>Name</label>
+			    <input type="text" value={Name} onChange={onNameHandler} />
+			    
+			    <label>Password</label>
+			    <input type="password" value={Password} onChange={onPasswordHandler} />
+			    
+			    <label>Confirm Password</label>
+			    <input type="password" value={confirmPassword} onChange={onConfirmPasswordHandler} />
+			    <br />
+			    <button type="submit">
+				Sign Up
+			    </button>
+			</form>
+		    </div>
+	)
 }
 
-export default RegisterPage;
+export default withRouter(RegisterPage)
