b5cd47b104d96b922b90db3cb31007db12394af1
sosow0212 <sosow0212@naver.com>
2021-09-23 16:55:47 +0900

    로그아웃 완성

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get('/api/hello')
     .then(response => {
@@ -9,12 +9,28 @@ function LandingPage() {
     })
   }, [])
 
+  
+  function onClickHandler() {
+    axios.get('/api/users/logout')
+    .then(response => {
+      if(response.data.success) {
+        props.history.push("/login")
+      } else {
+        alert('로그아웃 하는데 실패 했습니다.')
+      }
+    })
+  }
+
   return (
     <div style={{
       display:'flex', justifyContent:'center', alignItems:'center',
       width:'100%', height:'100vh'
     }}>
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>
+        로그아웃
+      </button>
     </div>
   )
 }
---
+++ client/src/hoc/auth.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function(SpecificComponent, option, adminRout = null) {
+
+
+
+
+
+  return AuthenticationCheck
+}
